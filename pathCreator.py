bfe556c4ca737efaac63bc3c8cb6cc8c7d2bbd20
jpshade <jpshade@csu.fullerton.edu>
2016-09-03 11:09:25 -0700

    Add anomaly checking

--- pathCreator.py
+++ pathCreator.py
@@ -4,6 +4,13 @@ initialize()
 
 paths = []
 
+lengthAnomalies = []
+
+prematurePaths = []
+
+def isPowerOfTwo(num):
+	return bin(num).count('1') == 1
+
 def generatePath(firstElement, secondElement):
 	
 	pathList = ['e']
@@ -23,15 +30,40 @@ def generatePath(firstElement, secondElement):
 	for element in pathList:
 		pathString = pathString + element + ' -> '
 
+
 	pathString = pathString[:-4]
 
 	fullString =  '(' + firstElement + ' , ' + secondElement + ') : ' +  str(counter) + ' steps : ' + pathString
 
-	return {'elements' : (firstElement, secondElement), 'pathList' : pathList, 'counter' : counter, 'fullString': fullString}
+	pathDict = {'elements' : (firstElement, secondElement), 'pathList' : pathList, 'counter' : counter, 'fullString': fullString}
 
-with open('D' + str(n) + 'Results.txt', 'w') as f:
+	if (not isPowerOfTwo(pathDict['counter']) and (order % pathDict['counter'])):
+		lengthAnomalies.append(pathDict)
+
+	if (firstElement != 'e') and (secondElement != 'e') and (pathDict['pathList'][-1] != 'e'):
+		prematurePaths.append(pathDict)
+
+	return pathDict
+
+
+
+
+
+
+with open('D' + str(order) + 'Results.txt', 'w') as f:
 	f.write('Results:\n\n')
 
 	for firstElement in myGroup:
 		for secondElement in myGroup:
 			f.write(generatePath(firstElement,secondElement)['fullString'] + '\n')
+
+with open('D' + str(order) + 'Anomalies.txt', 'w') as f:
+	f.write('Length Anomalies:\n\n')
+
+	for path in lengthAnomalies:
+		f.write(path['fullString'] + '\n')
+
+	f.write('\n\nPremature Paths\n\n')
+
+	for path in prematurePaths:
+		f.write(path['fullString']+'\n')
